eaf1a894208ad7750aa1ae67853a99fa9d0e352c
Manideep Naidu <manideepnaidugorle@gmail.com>
2025-06-25 01:57:09 +0530

    add product filters

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,20 +1,31 @@
-import { Breadcrumb, Flex, Space } from "antd"
-import {RightOutlined} from '@ant-design/icons'
+import { Breadcrumb, Button, Flex, Space , Form } from "antd"
+import {PlusOutlined, RightOutlined} from '@ant-design/icons'
 import { Link } from "react-router-dom"
+import ProductsFilter from "./ProductsFilter"
 
 const Products = () => {
-  return (
-    <>
-        <Space direction="vertical" size="large" style={{ width: '100%' }}>
-            <Flex justify='space-between'>
-                    <Breadcrumb
-                    separator={<RightOutlined />}
-                    items={[{ title: <Link to="/">Dashboard</Link> }, { title: 'Products' }]}
-                />
-            </Flex>
-        </Space>
-    </>
-  )
+    const [filterForm] = Form.useForm();
+
+    return (
+        <>
+            <Space direction="vertical" size="large" style={{ width: '100%' }}>
+                <Flex justify='space-between'>
+                        <Breadcrumb
+                        separator={<RightOutlined />}
+                        items={[{ title: <Link to="/">Dashboard</Link> }, { title: 'Products' }]}
+                    />
+                </Flex>
+            </Space>
+
+            <Form form={filterForm} onFieldsChange={() => {}}>
+                <ProductsFilter>
+                    <Button type="primary" icon={<PlusOutlined />} onClick={() => {}}>
+                        Add Product
+                    </Button>
+                </ProductsFilter>
+            </Form>
+        </>
+    )
 }
 
 export default Products
