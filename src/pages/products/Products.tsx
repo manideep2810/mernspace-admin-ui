0905dc6c773688ef9434ea0412e4f796d6ab51bc
Manideep Naidu <manideepnaidugorle@gmail.com>
2025-06-25 01:41:58 +0530

    add breadcrumb

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,6 +1,19 @@
+import { Breadcrumb, Flex, Space } from "antd"
+import {RightOutlined} from '@ant-design/icons'
+import { Link } from "react-router-dom"
+
 const Products = () => {
   return (
-    <div>Products</div>
+    <>
+        <Space direction="vertical" size="large" style={{ width: '100%' }}>
+            <Flex justify='space-between'>
+                    <Breadcrumb
+                    separator={<RightOutlined />}
+                    items={[{ title: <Link to="/">Dashboard</Link> }, { title: 'Products' }]}
+                />
+            </Flex>
+        </Space>
+    </>
   )
 }
 
